a0ad3f74de9668a35a4cbaec4056e3393d43c7ac
nik012003 <guerrera.nicola@gmail.com>
2023-09-28 18:02:05 +0200

    first commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 Mr Beam Lasers
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+// LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+
+int main() {
+  return 0;
+}
